4ef7acd9e6cad2da3d9e4a54a54b9cb433befede
Lucas Falcão <lucasfalcao@fabricainfo.com>
2020-11-05 18:13:26 -0300

    Setup Google API key.

--- src/screens/map/Map.tsx
+++ src/screens/map/Map.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native';
 
 class MapScreen extends React.Component<any, any> {
-    render() {
-        return <></>
-    }
+  render() {
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        
+      </SafeAreaView>
+    );
+  }
 }
 
 export default MapScreen;
