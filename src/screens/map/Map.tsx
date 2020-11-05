a0db43fffc047bad8eb5ecf6516dbe716ce24349
Lucas Falcão <lucasfalcao@fabricainfo.com>
2020-11-05 18:19:15 -0300

    Map working.

--- src/screens/map/Map.tsx
+++ src/screens/map/Map.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 class MapScreen extends React.Component<any, any> {
   render() {
     return (
       <SafeAreaView style={{ flex: 1 }}>
-        
+        <MapView style={styles.map}/>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  map: {
+    position: "absolute", 
+    top: 0, left: 0, right: 0, bottom: 0 
+  }
+})
 
 export default MapScreen;
